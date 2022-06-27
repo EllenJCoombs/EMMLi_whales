346db9f2db44944447fddb131a214a2a3cdee8a9
EllenJCoombs <ellen.coombs.14@ucl.ac.uk>
2022-06-27 15:55:30 -0400

    Update and rename EMMLi to EMMLi.R

--- EMMLi.R
+++ EMMLi.R
@@ -1,15 +1,17 @@
 
 
-# create vector congruence coefficient correlation matrix
+#create vector congruence coefficient correlation matrix
 VCV <-dotcorr (neurocranium.nosymm.geomorph$coords)
 
-# save VCV
+#save VCV
 save(VCV, file = 'VCV.R')
 
-# import point by point module definition file
+#import point by point module definition file
 module.df <- as.data.frame(read.csv("D:/modularity guide.csv"))
 
-# Run EMMLi (second input is number of specimens in sample)
+#Run EMMLi (second input is number of specimens in sample)
 EMMLi(VCV,62,module.df, saveAs = ("D:/Current/NHM/data/Pelagiaria/Modularity/neurocranium_EMMLi.csv"), pprob = 0.05)
 
 #you'll need to create a .csv file ("modularity guide.csv") listing all your hypotheses (see .csv)
+#The numbers in the columns correspond to the module in that hypothesis, e.g. all the landmarks belonging to module 1 have a number 1 in the relevant column
+
